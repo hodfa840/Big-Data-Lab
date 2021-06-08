4a034fc37ca7ee05e54f97a6cbb499534bbe467a
Hoda Fakharzadehjahromy <hodfa840@student.liu.se>
2021-06-08 01:20:53 +0000

    Replace ex5.py

--- lab1-spark/code/ex5.py
+++ lab1-spark/code/ex5.py
@@ -1,6 +1,7 @@
 from pyspark import SparkContext
 import os
 import sys
+from operator import add
 
 os.environ['PYSPARK_PYTHON'] = sys.executable
 os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
@@ -8,27 +9,24 @@ os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
 # create the spark application
 sc = SparkContext(appName="exe")
 
-precipitation_file = sc.textFile("precipitation-readings.csv")
 
-# transform the data by splitting each line
-lines_precipitation = precipitation_file.map(lambda line: line.split(";"))
-#print(lines_precipitation.take(5))
-# import stations in Ostergotland
-Ostergotland = sc.textFile("stations-Ostergotland.csv")
+ostergotland = sc.textFile('data/stations-Ostergotland.csv')
+stations = ostergotland.map(lambda line: line.split(";")[0]).collect()
+stations = sc.broadcast(stations)
+#print(stations.value)
+precipitation = sc.textFile("data/precipitation-readings.csv").cache()
+precipitation = precipitation.map(lambda x: x.split(';') )
+precipitation1 = precipitation.filter(lambda x: x[0] in stations.value)
 
-# transform the data by splitting each line
-lines_ost = Ostergotland.map(lambda line: line.split(";"))
-# list od Ostergotland station
-Ostergotland_ids = lines_ost.map(lambda x: x[0]).collect()
+precipitation1 = precipitation1.filter(lambda x: int(x[1][0:4]) >= 1993 and int(x[1][0:4]) <= 2016)
+precipitation1 = precipitation1.map(lambda x: ((x[0],x[1][0:7]),float(x[3])))
 
-# map as ((station_no, yyyy, mm), (precipitation, 1))
-# sum all values by reduceByKey
-# map it again to find avg of month
-# sort
-ost_station_prec = lines_precipitation.map(lambda x: ((x[0], x[1][0:4], x[1][5:7]), (float(x[3]), 1))). \
-    filter(lambda x: x[0][0] in Ostergotland_ids and int(x[0][1]) > 1993 and int(x[0][1]) < 2016).map(
-    lambda x: ((x[0][1], x[0][2]), x[1])). \
-    reduceByKey(lambda x, y: (x[0] + y[0], x[1] + y[1])).map(lambda x: (x[0], x[1][0] / x[1][1])).sortByKey(False)
+#calculate the monthly precipitation for each station
+precipitation1 = precipitation1.reduceByKey(add)
 
-ost_station_prec.saveAsTextFile("./res/ex5")
-print(ost_station_prec.collect())
+tmp = precipitation1.map(lambda x: (x[0][1],  (x[1], 1)))
+tmp = tmp.reduceByKey(lambda x,y: (x[0]+y[0],x[1]+y[1]))
+#print(tmp.collect())
+precip_ave = tmp.map(lambda x: (x[0],round(x[1][0]/x[1][1],ndigits=3)))
+# #precip_ave.saveAsTextFile("assignment5_updated")
+print(precip_ave.collect())
