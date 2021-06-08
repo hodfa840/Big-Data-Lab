63b38ff07de3993e1d46db7cfef91764e98fcb01
Hoda Fakharzadehjahromy <hodfa840@student.liu.se>
2021-06-08 01:17:40 +0000

    Replace ex4.py

--- lab1-spark/code/ex4.py
+++ lab1-spark/code/ex4.py
@@ -1,36 +1,37 @@
-# import pyspark
 from pyspark import SparkContext
 import os
 import sys
+from operator import add
 
 os.environ['PYSPARK_PYTHON'] = sys.executable
 os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
-# create the spark application
-sc = SparkContext(appName = "ex4")
-# import the data
-temperature = sc.textFile("data/temperature-readings.csv")
-# transform the data by splitting each line
-linesT = temperature.map(lambda line: line.split(";"))
-# import the data
-precipitation = sc.textFile("data/precipitation-readings.csv")
-# transform the data by splitting each line
-linesP= precipitation.map(lambda line: line.split(";"))
-############
-##### 4
-############
-# map as (station_no, temp)
-# find maximum read of station
-station_temp= linesT. map(lambda x: (x[0], float(x[3]))).reduceByKey(max).sortByKey()
+sc = SparkContext(appName='ex')
 
-station_temp2 = station_temp.filter(lambda x: x[1] > 20 and x[1] < 30 )
-print(station_temp.take(20))
-# calculate daily precipitation
-# # map as (station, precipitation)
-station_precip= linesP. map(lambda x: (x[0],float(x[3]))).reduceByKey(max).sortByKey()
-station_precip=station_precip.filter(lambda x: x[1]>100 and x[1]< 200)
-print("------------------------------------------------------------------------------------------------------------")
-print(station_precip.take(20))
-station_precip_temp = station_temp.join(station_precip)
-#
-#
-station_precip_temp.saveAsTextFile("./res/ex4")
+temperature = sc.textFile("data/temperature-readings.csv").cache()
+temperature = temperature.map(lambda a: a.split(';'))
+temperature = temperature.map(lambda x: (x[0],float(x[3])))
+temperature = temperature.reduceByKey(max)
+temperature = temperature.filter(lambda x: x[1] >= 25 and x[1] <= 30 )
+
+#print(temperature.collect())
+
+
+precipitation = sc.textFile("data/precipitation-readings.csv").cache()
+precipitation = precipitation.map(lambda x: x.split(';') )
+
+precipitation = precipitation.map(lambda x: ((x[0],x[1]),float(x[3])))
+#calculating daily precipitation
+precipitation1 = precipitation.reduceByKey(add)
+precipitation1=precipitation1.map(lambda x: (x[0][0],x[1]))
+#finding daily precipitation
+precipitation1 = precipitation1.reduceByKey(max)
+precipitation1 = precipitation1.filter(lambda x: x[1] >= 100 and x[1] <= 200)
+
+#print(precipitation1.collect())
+
+
+# join the RDDs
+stations = temperature.join(precipitation1)
+# #the results
+stations.saveAsTextFile("assignment4_updated")
+print(stations.collect())
